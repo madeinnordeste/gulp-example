630fa3d60db62b51cd5572954d857a8e5ef92417
Luiz Alberto S. Ribeiro <madeinnordeste@gmail.com>
2016-01-25 20:00:52 -0300

    minify and concat css

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,12 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var concatcss = require('gulp-concat-css');
+var cssreplace = require('gulp-replace');
+var cssnano = require('gulp-cssnano');
 
+//minify and concat javascripts
 gulp.task('scripts-minify-concat', function() {
-    // corpo da tarefa 
     return gulp
             .src(['src/js/**/*.js'])
             .pipe(uglify())
@@ -13,8 +16,20 @@ gulp.task('scripts-minify-concat', function() {
             .pipe(gulp.dest('dist/js'));      
 });
 
+//minify and concat css
+gulp.task('styles-minify-concat', function() {
+     return gulp
+	 		.src('src/css/**/*.css')
+	 		.pipe(concatcss("style.css"))
+	 		.pipe(cssreplace('/*!', '/*'))
+	 		.pipe(cssnano())
+	    	.pipe(gulp.dest('dist/css'));
+    	
+});
+
+
 gulp.task('default', function() {
 
-	gulp.start('scripts-minify-concat');
+	gulp.start('scripts-minify-concat', 'styles-minify-concat');
 
 });
